86934b6cb15b2a99ff64a04fd8f2cb8db1888b9b
MakarimZufar <makarim.zufar@gmail.com>
2025-04-17 21:04:00 +0700

    feat: change bg color and change container size

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,8 @@
-import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function ContactPage() {
     return (
-        <main className="max-w-xl mx-auto px-6 py-20">
+        <main className="flex flex-col items-center justify-center min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
             <h1 className="text-3xl font-bold mb-6 text-center">
                 Hubungi Saya
             </h1>
@@ -14,7 +14,7 @@ export default function ContactPage() {
             <form
                 action="https://formsubmit.co/381995bdbef10eea6fb5b9fdf7bec572 "
                 method="POST"
-                className="flex flex-col gap-4 bg-black p-6 rounded-lg shadow-md"
+                className="flex flex-col gap-6 bg-black p-6 rounded-lg shadow-md w-1/2 max-w-4xl" // Mengatur lebar form
             >
                 {/* Redirect setelah submit */}
                 <input
@@ -31,20 +31,20 @@ export default function ContactPage() {
                     name="name"
                     placeholder="Nama Kamu"
                     required
-                    className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
                 />
                 <input
                     type="email"
                     name="email"
                     placeholder="Email Kamu"
                     required
-                    className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
                 />
                 <textarea
                     name="message"
                     placeholder="Pesan Kamu"
                     required
-                    className="border border-gray-300 rounded px-4 py-2 h-32 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="border border-gray-300 rounded px-4 py-2 h-32 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
                 />
                 <button
                     type="submit"
@@ -53,6 +53,7 @@ export default function ContactPage() {
                     Kirim Pesan
                 </button>
             </form>
+
             {/* Bagian ikon sosmed */}
             <div className="flex justify-center gap-6 mt-10 text-2xl text-gray-600">
                 <a
