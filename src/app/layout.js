a6f9197ae34cde96d10777811306b6a1a0152ddb
MakarimZufar <makarim.zufar@gmail.com>
2025-04-17 21:03:20 +0700

    feat: remove top padding and change title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,30 +4,29 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "My Porotofolio",
-  description: "Created with Next.js + Tailwind",
+    title: "Makarim Zufar Portofolio",
+    description: "Created with Next.js + Tailwind",
 };
 
 export default function RootLayout({ children }) {
-  return (
-      <html lang="en">
-          <body className="bg-white text-black dark:bg-gray-900 dark:text-white flex flex-col min-h-screen">
-                  <Navbar />
-                  <div className="flex-grow pt-16">
-                      {children} 
-                  </div>
-                  <Footer /> 
-          </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <body className="bg-white text-black dark:bg-gray-900 dark:text-white flex flex-col min-h-screen">
+                <Navbar />
+                <div className="flex-grow pb-6">{children}</div>{" "}
+                {/* Padding bawah untuk mencegah overlap dengan footer */}
+                <Footer />
+            </body>
+        </html>
+    );
 }
