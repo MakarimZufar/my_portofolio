f51a5b9b6db3aaed20026a59e9b639f34f1a19e7
MakarimZufar <makarim.zufar@gmail.com>
2025-04-17 21:13:54 +0700

    style: settings padding and background color

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,6 @@
 export default function AboutPage() {
     return (
-        <main className="max-w-3xl mx-auto py-10 px-4">
+        <main className="pt-30 flex flex-col items-center min-h-screen px-6 sm:px-20 py-16 gap-6 text-center bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
             <h1 className="text-3xl font-bold mb-4 text-center">
                 Tentang Saya
             </h1>
