8bbc01d055f2415778335c7c892b4574e758d0ed
MakarimZufar <makarim.zufar@gmail.com>
2025-05-06 20:49:16 +0700

    del: un used file

--- src/components/Projects/index.js
+++
@@ -1,5 +0,0 @@
-// Mengimpor komponen dari common folder alih-alih lokal
-import ProjectCard from "@/components/common/ProjectCard";
-import ProjectDetail from "@/components/common/ProjectDetail";
-
-export { ProjectCard, ProjectDetail };
